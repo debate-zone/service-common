689635cbf33b85684470894c5eac20a7896d107f
Grigore Theodor <grigore.theodor.997@gmail.com>
2023-06-09 13:19:45 +0000

    Task: Collection enums, zod IDObject schema, fix db return create, prepare for deploy

--- src/enums/collectionsEnum.ts
+++ src/enums/collectionsEnum.ts
@@ -1,8 +1,8 @@
 export enum CollectionsEnum {
-  POLITICAL_PREFERENCE_HISTORY = "politicalPreferenceHistory",
-  DEBATE_ZONE = "debateZone",
-  USER = "user",
-  NOTIFICATION = "notification",
-  DISCOURSE_DEFENDER = "discourseDefender",
-  STREAMING = "streaming"
+    POLITICAL_PREFERENCE_HISTORY = 'politicalPreferenceHistory',
+    DEBATE_ZONE = 'debateZone',
+    USER = 'user',
+    NOTIFICATION = 'notification',
+    DISCOURSE_DEFENDER = 'discourseDefender',
+    STREAMING = 'streaming',
 }
--- src/zod/baseZodSchema.ts
+++ src/zod/baseZodSchema.ts
@@ -1,32 +1,32 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
-export const idObjectIdsSchema = z.any().transform((val) => {
+export const idObjectIdsSchema = z.any().transform(val => {
     if (typeof val === 'string') {
-        return val
+        return val;
     }
     if (typeof val === 'object' && val !== null) {
-        return val.toString()
+        return val.toString();
     }
-    return val
-})
+    return val;
+});
 
 export const baseZodSchema = z
-  .object({
-    _id: idObjectIdsSchema,
-    __v: z.number().optional(),
-    isDeleted: z.boolean().default(false),
-    createdAt: z.date().optional(),
-    updatedAt: z.date().optional(),
-  })
-  .strict()
+    .object({
+        _id: idObjectIdsSchema,
+        __v: z.number().optional(),
+        isDeleted: z.boolean().default(false),
+        createdAt: z.date().optional(),
+        updatedAt: z.date().optional(),
+    })
+    .strict();
 
-export const newBaseZodSchema = baseZodSchema.deepPartial()
+export const newBaseZodSchema = baseZodSchema.deepPartial();
 
-export const emailSchema = z.string().email().trim().min(5).max(50)
+export const emailSchema = z.string().email().trim().min(5).max(50);
 
 export const phoneNumberSchema = z
-  .string()
-  .regex(
-    new RegExp('^\\+373(\\d{2,3})?\\d{5,6}$'),
-    'Phone number must be in format +373xxxxxxxx'
-  )
+    .string()
+    .regex(
+        new RegExp('^\\+373(\\d{2,3})?\\d{5,6}$'),
+        'Phone number must be in format +373xxxxxxxx',
+    );
